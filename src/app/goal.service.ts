d09910fd43fac8eb37101efc547abf744ba0d927
ingabire <ingabire@sylvie.gmail.com>
2019-02-04 17:08:58 +0200

    i had created a navigation bar in our appComponent template file to display different routes

--- src/app/goal.service.ts
+++ src/app/goal.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from "@angular/core";
 
 @Injectable({
   providedIn: "root"
-// })
-// export class GoalService {
-//   getGoals() {
-//     // return Goal;
+})
+export class GoalService {
+  getGoals() {
+    // return Goal;
 //   }
 
 //   constructor() {}
